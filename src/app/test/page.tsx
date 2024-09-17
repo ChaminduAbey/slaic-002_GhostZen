4891715bf90834d62c9f0ad1a3d8f0827c96dc90
ChaminduAbey <chaminduabey@gmail.com>
2024-09-17 10:47:05 +0530

    1. election details done

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,9 @@
+import { ElectionDetailsView } from "@/components/election-details";
 import { ManifestoComparator } from "@/components/manifesto-comparator";
 import { PollCard } from "@/components/poll/poll-card";
 
 export default function Page() {
     return <div className="flex max-w-4xl mx-auto px-5">
-        <ManifestoComparator />
+        <ElectionDetailsView />
     </div>
 }
